4ea27c8b7b0d08dbd34ddcb3aa1fe0a029b826ea
frparu <frparu@floridauniversitaria.es>
2021-05-13 16:49:31 +0200

    cambios fran

--- SportShare/Clase/Usuario.cs
+++ SportShare/Clase/Usuario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SportShare.Clases;
+using SportShare.Clase;
 
 namespace SportShare.Clase
 {
@@ -25,9 +25,24 @@ namespace SportShare.Clase
         private string enfermedades;
         private string valoracion;
 
-        public string Idusuario { get { return idUsuario;  } set { idUsuario = value;  } }
+        public string Idusuario { get { return idUsuario; } set { idUsuario = value;  } }
         public string Nombreusuario { get { return nombreusuario; } set {  nombreusuario = value;  } }
         public string Contraseña { get { return contraseña; } set { contraseña = value;  } }
         public string Nombre { get { return nombre; } set { nombre = value;  } }
+        public string Apellidos { get { return apellidos; } set { apellidos = value; } }
+        public DateTime Fechanacimiento { get { return fechaNacimiento;  } set { fechaNacimiento = value;  } }
+        public double Peso { get { return peso; } set { peso = value;  } }
+        public double Altura { get { return altura;  } set { altura = value;  } }
+
+
+
+
+
+
+
+
+
+
+        public int RegistrarUsuario(co)
     }
 }
