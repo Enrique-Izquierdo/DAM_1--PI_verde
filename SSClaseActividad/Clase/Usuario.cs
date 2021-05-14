e97e4ce1cac877ae639dd3ac42f2af2a6caef88f
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-05-14 14:44:23 +0200

    clase Actividad con 2 constructores (sobrecargado), 1 método de agregación, 1 método de busqueda y 3 métodos de eliminación (sobrecargados). Nota: la clase Actividad se ha creado sobre una versión antigua del proyecto.

---
+++ SSClaseActividad/Clase/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportShare.Clase;
+
+namespace SportShare.Clase
+{
+    class Usuario
+    {
+        private string idUsuario;
+        private string nombreusuario;
+        private string contraseña;
+        private string nombre;
+        private string apellidos;
+        private DateTime fechaNacimiento;
+        private double peso;
+        private double altura;
+        private string email;
+        private int telefono;
+        private string poblacion;
+        private string provincia;
+        private string deportePreferido;
+        private string enfermedades;
+        private string valoracion;
+
+        public string Idusuario { get { return idUsuario; } set { idUsuario = value;  } }
+        public string Nombreusuario { get { return nombreusuario; } set {  nombreusuario = value;  } }
+        public string Contraseña { get { return contraseña; } set { contraseña = value;  } }
+        public string Nombre { get { return nombre; } set { nombre = value;  } }
+        public string Apellidos { get { return apellidos; } set { apellidos = value; } }
+        public DateTime Fechanacimiento { get { return fechaNacimiento;  } set { fechaNacimiento = value;  } }
+        public double Peso { get { return peso; } set { peso = value;  } }
+        public double Altura { get { return altura;  } set { altura = value;  } }
+
+
+    }
+}
